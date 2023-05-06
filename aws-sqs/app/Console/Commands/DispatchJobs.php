c45837faf89e015b2897022d46380bb969e359bb
Antoine Benevaut <me@abenevaut.dev>
2023-05-06 10:45:58 +0200

    exp(aws-sqs): Initial commit

---
+++ aws-sqs/app/Console/Commands/DispatchJobs.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SqsFifoJob;
+use App\Jobs\SqsStandardJob;
+use Illuminate\Console\Command;
+
+class DispatchJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dispatch:jobs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dispatch experimental jobs';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        SqsStandardJob::dispatch()->onQueue(env('SQS_QUEUE'));
+
+
+        SqsFifoJob::dispatch()
+            ->onConnection('sqs-fifo')
+            ->onQueue(env('SQS_QUEUE_FIFO'));
+
+
+
+
+
+        return Command::SUCCESS;
+    }
+}
