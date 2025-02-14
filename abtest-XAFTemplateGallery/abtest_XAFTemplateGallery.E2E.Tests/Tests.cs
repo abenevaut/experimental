dc963113029d4d54323fe4376cb74ca42fb4bc17
Antoine Benevaut <me@abenevaut.dev>
2025-02-15 00:21:31 +0100

    initial commit xaf project + docker-compose

---
+++ abtest-XAFTemplateGallery/abtest_XAFTemplateGallery.E2E.Tests/Tests.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace abtest_XAFTemplateGallery.Module.E2E.Tests;
+
+public class abtest_XAFTemplateGalleryTests : IDisposable {
+    const string BlazorAppName = "abtest_XAFTemplateGalleryBlazor";
+    const string AppDBName = "abtest_XAFTemplateGallery";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+    public abtest_XAFTemplateGalleryTests() {
+        FixtureContext.RegisterApplications(
+            new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\abtest_XAFTemplateGallery.Blazor.Server", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "abtest_XAFTemplateGalleryEasyTest", server: @"(localdb)\mssqllocaldb"));
+    }
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(BlazorAppName)]
+    public void TestBlazorApp(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+        appContext.GetForm().FillForm(("User Name", "Admin"));
+        appContext.GetAction("Log In").Execute();
+        Assert.True(appContext.Navigate("My Details"));
+        Assert.True(appContext.Navigate("Role"));
+        Assert.True(appContext.Navigate("Users"));
+    }
+}
